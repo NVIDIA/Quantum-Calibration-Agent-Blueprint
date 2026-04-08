17ea01e37ae832e7a3e410acbf642876d94bab1b
mdzurick <mdzurick@nvidia.com>
2026-04-08 10:54:15 -0700

    update license
    Signed-off-by: mdzurick <mdzurick@nvidia.com>

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,17 +1,17 @@
-# Copyright 2026 NVIDIA CORPORATION & AFFILIATES
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 #
-# Licensed under the NVIDIA Software and Model Evaluation License (the "License");
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# SPDX-License-Identifier: LicenseRef-NvidiaProprietary
 
 """Mock experiments with realistic wall-clock execution time for workflow testing."""
 
